d7137dc7f5c0dafeadbbe5e9d4557c3ad7cdfeef
weather0 <af1291309@gmail.com>
2022-05-03 17:04:47 +0900

    JDBC

---
+++ P220503/src/main/java/co/micol/notice/App.java
@@ -0,0 +1,25 @@
+package co.micol.notice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import co.micol.notice.service.NoticeService;
+import co.micol.notice.service.NoticeVO;
+import co.micol.notice.serviceimpl.NoticeServiceImpl;
+
+public class App {
+	public static void main(String[] args) {
+		Menu m = new Menu();
+		m.run();
+		
+		
+		
+
+		// 전체 조회
+
+
+		// 한 명 조회
+
+
+	}
+}
